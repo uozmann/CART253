2e5f1a37bfb6416d3bc114812809c3fff69e7445
uozmann <1974155655@qq.com>
2021-09-23 23:29:53 -0400

    Exercice 1 canvas size change

--- exercises/i-like-to-move-it/js/script.js
+++ exercises/i-like-to-move-it/js/script.js
@@ -1,7 +1,6 @@
 /**
 Exercise1: I like to move it
 Man Zou
-
 Include three shapes: arc, triangle, rectancle and ellipse.
 Include movement: arc moving on x axis.
 Include size changes: arc changing its size.
@@ -30,10 +29,6 @@ let paintCanvas = {
   width: 1800,
   height: 500,
 }
-// RGB colour settings input
-let randomNumber=random(0, 5);
-let greenMouse = map(mouseX, 0, width, 0, 255);
-let blueMouse = map(mouseY, 0, width, 0, 255);
 
 // Paintbrush and colour-picker object
 let circle = {
@@ -46,12 +41,12 @@ let circle = {
 
 // Canvas indication
 let indication = {
-  x1: 200,
-  y1: 750,
-  x2: 150,
-  y2: 650,
-  x3: 250,
-  y3: 650,
+  x1: 100,
+  y1: 650,
+  x2: 50,
+  y2: 550,
+  x3: 150,
+  y3: 550,
   size: 100,
   speed: 0.5,
   fill: 0
@@ -59,9 +54,9 @@ let indication = {
 
 // Animated half-circle object
 let moon = {
-  x: 1500,
+  x: 800,
   y: 250,
-  size: 400,
+  size: 300,
   speed: 1,
   fill: 255,
   angle: 1
@@ -69,7 +64,7 @@ let moon = {
 
 
 function setup() {
-  createCanvas(1800, 1000);
+  createCanvas(1000, 800);
   background(backgroundShade);
 
   fill(paintCanvas.fill);
@@ -79,6 +74,9 @@ function setup() {
 
 function draw() {
 // Paintbrush colour variation settings
+  let randomNumber=random(0, 5);
+  let greenMouse = map(mouseX, 0, width, 0, 255);
+  let blueMouse = map(mouseY, 0, width, 0, 255);
   fill(200, greenMouse, blueMouse);
   noStroke();
   circle.x = mouseX;
@@ -87,11 +85,9 @@ function draw() {
 
 // Animated half-circle object settings
   moon.x += -1;
-  moon.x = constrain(moon.x, 200, 1800);
-  moon.x += -1;
-  moon.x = constrain(moon.x, 200, 1800);
+  moon.x = constrain(moon.x, 100, 1800);
   moon.size += -1;
-  moon.size = constrain(moon.size, -400, 400);
+  moon.size = constrain(moon.size, -300, 300);
   arc(moon.x, moon.y, moon.size, moon.size, PI + HALF_PI, HALF_PI);
 
 // Indication for painting area
