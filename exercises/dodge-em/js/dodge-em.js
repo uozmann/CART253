c00b708c2b8a6f22069c3cda854eb14d03227197
uozmann <1974155655@qq.com>
2021-10-03 21:25:33 -0400

    small corrections

--- exercises/dodge-em/js/dodge-em.js
+++ exercises/dodge-em/js/dodge-em.js
@@ -1,9 +1,8 @@
 // Exercise1: Dodge-em
 // Man Zou
-// Change the way the user controls their circle:
-// Add at least one new if-statement:
-// Change the way the simulation looks:
-// Use at least one image:
+// Change the way the user controls their circle: keyboard controls
+// Add at least one new if-statement: in function draw
+// Change the way the simulation looks: changed colours, elements, and shapes
 
 "use strict";
 
@@ -27,7 +26,7 @@ let character = {
   fill:0
 };
 
-// Canvas indication
+// Points indication
 let indication = {
   x: undefined,
   y: undefined,
@@ -37,8 +36,7 @@ let indication = {
   fill: 0
 }
 
-// Animated half-circle object
-
+// Keyboard Commands
 let controlOptions = {
   top: false,
   down: false,
@@ -49,7 +47,6 @@ let controlOptions = {
 
 function setup() {
   createCanvas(1000, 800);
-
 }
 
 
@@ -57,6 +54,7 @@ function draw() {
   background(backgroundShade.r, backgroundShade.g, backgroundShade.b);
   fill(indication.fill);
 
+  // The moving circle and settings to make it move according to keyboard commands
   push();
   if (controlOptions.left) {
     character.x += 1;
@@ -85,6 +83,7 @@ function draw() {
   ellipse(character.x, character.y, character.size);
   pop();
 
+  // Control options for drawing a line to the true point
   push();
   if (controlOptions.showLine) {
     stroke(0);
@@ -95,6 +94,7 @@ function draw() {
   line(character.x, character.y, indication.x, indication.y);
   pop();
 
+  // Drawing true and fake points
   push();
   randomSeed(0);
   for (let i = 0; i < indication.segments; i++) {
@@ -107,9 +107,9 @@ function draw() {
     noLoop();
   }
   pop();
-
 }
 
+// Key Settings
 function keyPressed() {
   if (key === 'a') {
     controlOptions.right = true;
