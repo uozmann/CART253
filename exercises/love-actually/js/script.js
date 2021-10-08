5afc9f8eccd2fba24bceec3766ad8da37036f6a7
uozmann <1974155655@qq.com>
2021-10-07 23:04:23 -0400

    Update script.js

--- exercises/love-actually/js/script.js
+++ exercises/love-actually/js/script.js
@@ -1,33 +1,43 @@
 // Exercise3: Love-actually
 // Man Zou
-// Change the way the user controls their circle:
-// Add at least one new if-statement:
-// Change the way the simulation looks:
-// Use at least one image:
+// The goal: enter in the big white circle (lover) while avoiding to touch the blue circle (ennemy)
+
+// Allow the user to control one of the circles: white circle controlled with mouse
+// Make the non-user circle move differently: the ennemy circle (blue) moves up and down (Perlin noise); the lover circle moves in the round (cos and sin functions)
+// Add at least one extra function: title, game, ending, ending2
+// Add at least one extra ending: 2 ending, one winner ending and one loser ending
 
 "use strict";
+// Fonts
 let cairoBlack;
 let cairoRegular;
+// Background image
 let bg;
+// Time control
 let t = 0;
+// Initial state
 let state = `title`;
+// Player circle controlled with the mouse
 let player ={
   x: undefined,
   y: undefined,
   size: 100,
 }
+// Ennemy circle moving up and down controlled by Perlin noise values
 let ennemy = {
   x: undefined,
   y: undefined,
   vy:0,
   size: 200,
 }
+// Lover circle moving in circle, controlled with cos and sin functions
 let lover = {
   x: undefined,
   y: undefined,
   size: 300,
 }
 
+// Loading images and text font
 function preload() {
   cairoBlack = loadFont(`assets/fonts/Cairo/Cairo-Black.ttf`);
   cairoRegular = loadFont(`assets/fonts/Cairo/Cairo-Regular.ttf`);
@@ -86,7 +96,7 @@ function game(){
   noStroke();
 
   push();
-  // background white dots; code and math formulas inspired from P5 library at: https://p5js.org/examples/interaction-wavemaker.html
+  // moving background with white circles; code and math formulas inspired from P5 library at: https://p5js.org/examples/interaction-wavemaker.html
   for (let x = 0; x <= width; x += 50) {
     for (let y = 0; y <= height; y += 50) {
 
@@ -101,13 +111,13 @@ function game(){
   }
   t = t + 0.01; // code from P5 library ends here
   pop();
-
+  // Player position controlled by the mouse
   push();
   player.x = mouseX;
   player.y = mouseY;
   ellipse(player.x, player.y, player.size);
   pop();
-
+  // Lover's circle movements
   push();
   lover.x = width/2 + 100 * cos(2 * PI * t + 360);
   lover.y = height/2 + 100 * sin(2 * PI * t + 360);
@@ -116,14 +126,14 @@ function game(){
   noFill();
   ellipse(lover.x, lover.y, lover.size);
   pop();
-
+  // Ennemy's circle moving on y axis up and down according to Perlin Noise variables
   push();
   ennemy.y = map(noise(t), 0, 1, 0, height);
   ennemy.x = width/2;
   fill(40,40,114);
   ellipse(ennemy.x, ennemy.y, ennemy.size);
   pop();
-
+  // To see if the player is in the lover's circle and if it touches the ennemy
   let dPlayerEnnemy = dist(player.x, player.y, ennemy.x, ennemy.y);
   let dPlayerLover = dist(player.x, player.y, lover.x, lover.y);
   let dEnnemyLover = dist(ennemy.x, ennemy.y, lover.x, lover.y);
@@ -133,9 +143,9 @@ function game(){
   else if (dPlayerEnnemy < ennemy.size/2 + player.size/2) {
     state = `ending2`;
   }
-
 }
 
+// Good ending
 function ending(){
   background(255,255,255);
   fill(40,40,114);
@@ -156,6 +166,7 @@ function ending(){
   noLoop();
 }
 
+// Bad ending
 function ending2(){
   background(0,0,0);
   fill(40,40,114);
@@ -176,6 +187,7 @@ function ending2(){
   noLoop();
 }
 
+// Return to the title page
 function keyPressed() {
   if (key === 'r') {
     state = 'title';
