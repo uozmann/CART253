b222b3ad7377ac09928653d6fb1d528753127ce4
uozmann <1974155655@qq.com>
2021-10-16 20:42:37 -0400

    Made the background animation

--- projects/simulation-project1/js/functionscourse.js
+++ projects/simulation-project1/js/functionscourse.js
@@ -1,76 +1,37 @@
 "use strict";
 
-// Our fish
-let fish1;
-let fish2;
-let fish3;
-let fish4;
+let school = [];
+let schoolSize = 4;
 
 function setup() {
   createCanvas(600, 600);
 
-  // Create four fish, positioned randomly
-  fish1 = createFish(random(0, width), random(0, height));
-  fish2 = createFish(random(0, width), random(0, height));
-  fish3 = createFish(random(0, width), random(0, height));
-  fish4 = createFish(random(0, width), random(0, height));
-}
-
-// createFish(x,y)
-// Creates a new JavaScript Object describing a fish and returns it
-function createFish(x, y) {
-  let fish = {
-    x: x,
-    y: y,
-    size: 50,
-    vx: 0,
-    vy: 0,
-    speed: 2
-  };
-  return fish;
-}
-
-// draw()
-// Moves and displays our fish
-function draw() {
-  background(0);
-
-  moveFish(fish1);
-  moveFish(fish2);
-  moveFish(fish3);
-  moveFish(fish4);
-
-  displayFish(fish1);
-  displayFish(fish2);
-  displayFish(fish3);
-  displayFish(fish4);
-}
-
-// moveFish(fish)
-// Chooses whether the provided fish changes direction and moves it
-function moveFish(fish) {
-  // Choose whether to change direction
-  let change = random(0, 1);
-  if (change < 0.05) {
-    fish.vx = random(-fish.speed, fish.speed);
-    fish.vy = random(-fish.speed, fish.speed);
+  for (let i = 0; i < schoolSize; i++) {
+    // Create a fish
+    let fish = createFish(random(0, width), random(0, height));
+    // Add the fish to our array
+    school.push(fish);
   }
-
-  // Move the fish
-  fish.x = fish.x + fish.vx;
-  fish.y = fish.y + fish.vy;
-
-  // Constrain the fish to the canvas
-  fish.x = constrain(fish.x, 0, width);
-  fish.y = constrain(fish.y, 0, height);
 }
-
-// displayFish(fish)
-// Displays the provided fish on the canvas
-function displayFish(fish) {
-  push();
-  fill(200, 100, 100);
-  noStroke();
-  ellipse(fish.x, fish.y, fish.size);
-  pop();
+// mousePressed() checks whether a fish in the school was clicked
+// and removes it if so
+function mousePressed() {
+  // Use a for loop to examine every fish in the school one by one
+  for (let i = 0; i < school.length; i++) {
+    // Store the current fish in the fish variable
+    let fish = school[i];
+    // Calculate the distance between the mouse position and the fish
+    let d = dist(mouseX, mouseY, fish.x, fish.y);
+    // If the distance means the mouse was clicked inside the fish
+    if (d < fish.size / 2) {
+      // Remove the fish using the splice() function which takes two arguments
+      // - The index to start remove elements from (i in our case)
+      // - The number of elements to remove from that position (just one for us)
+      school.splice(i, 1);
+      // Now that we've found our fish to remove, we don't want to continue
+      // going through the loop, so we end it prematurely with break
+      // This forces the for-loop to stop immediately
+      break;
+    }
+  }
 }
--- projects/simulation-project1/js/script.js
+++ projects/simulation-project1/js/script.js
@@ -1,51 +1,52 @@
-// Exercise3: Love-actually
+// Project1: Life Story
 // Man Zou
-// The goal: enter in the big white circle (lover) while avoiding to touch the blue circle (ennemy)
-
-// Allow the user to control one of the circles: white circle controlled with mouse
-// Make the non-user circle move differently: the ennemy circle (blue) moves up and down (Perlin noise); the lover circle moves in the round (cos and sin functions)
-// Add at least one extra function: title, game, ending, ending2
-// Add at least one extra ending: 2 ending, one winner ending and one loser ending
 
 "use strict";
 // Fonts
 let cairoBlack;
 let cairoRegular;
-// Background image
-let bg;
-// Time control
-let t = 0;
-// Initial state
-let state = `title`;
-// Player circle controlled with the mouse
-let player ={
-  x: undefined,
-  y: undefined,
-  size: 100,
+let state = 'title'
+let bgTitle = {
+  r: 100,
+  g: 100,
+  b: 120,
+};
+
+let bgGame = {
+  r: 240, 
+  g: 220,
+  b: 160,
+};
+
+let bgEnding;
+let bgPoints = {
+  x: 0,
+  y: 0,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  nbr: 2000,
+  trail: [],
 }
-// Ennemy circle moving up and down controlled by Perlin noise values
-let ennemy = {
-  x: undefined,
-  y: undefined,
-  vy:0,
-  size: 200,
-}
-// Lover circle moving in circle, controlled with cos and sin functions
-let lover = {
-  x: undefined,
-  y: undefined,
-  size: 300,
+let bgAutoPoints = {
+  x: 0,
+  y: 0,
+  vx: 0,
+  vy: 0,
+  speed: 5,
+  nbr: 2000,
+  trail: [],
 }
 
 // Loading images and text font
 function preload() {
   cairoBlack = loadFont(`assets/fonts/Cairo/Cairo-Black.ttf`);
   cairoRegular = loadFont(`assets/fonts/Cairo/Cairo-Regular.ttf`);
-  bg = loadImage(`assets/images/cloudgirl-bg.jpg`)
+  bgGame = loadImage(`assets/images/cloudgirl-bg.jpg`)
 }
 
 function setup() {
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(1000, 700);
 }
 
 function draw() {
@@ -61,88 +62,77 @@ function draw() {
   case `ending`:
     ending();
     break;
-
-  case `ending2`:
-    ending2();
-    break;
   }
 }
 
 function title() {
-  background(bg);
+  background(bgTitle.r, bgTitle.g, bgTitle.b);
   fill(255);
   textAlign(CENTER, CENTER);
 
+  // Title text
   push();
   textFont(cairoBlack);
-  textSize(200);
-  text(`Isn't it lovely?`, width / 2, height / 3);
+  textSize(100);
+  text(`A simple life story`, width / 2, height / 3);
   pop();
 
+  // Start command text
   push();
   textFont(cairoRegular);
   textSize(24);
-  text(`Start`, width/5, height*3 / 4);
-  pop();
-
-  let dStartGame = dist(mouseX, mouseY, width/5, height*3 / 4);
+  text(`Hover here to play`, width/2, height*3 / 4);
+  let dStartGame = dist(mouseX, mouseY, width/2, height*3 / 4);
   if(dStartGame < 10) {
     state = 'game';
   }
+  pop();
+
+  // Background animation
+  push();
+  stroke(220);
+  strokeWeight(10);
+  for ( let i = 0; i < bgPoints.trail.length; i++ ) {
+    let pastPoints = bgPoints.trail[i];
+    point(pastPoints.x, pastPoints.y);
+  }
+  bgPoints.x = mouseX;
+  bgPoints.y = mouseY;
+  point(bgPoints.x, bgPoints.y);
+  let newTrailPosition = {
+    x: bgPoints.x, 
+    y: bgPoints.y,
+  };
+
+  bgPoints.trail.push(newTrailPosition);
+  pop();
 }
 
 function game(){
-  background(63,116,254);
-  noStroke();
+  background(bgGame.r, bgGame.g, bgGame.b);
+  background(240, 220, 220);
+  fill(255);
+  textAlign(CENTER, CENTER);
 
+  // Background animation 
   push();
-  // moving background with white circles; code and math formulas inspired from P5 library at: https://p5js.org/examples/interaction-wavemaker.html
-  for (let x = 0; x <= width; x += 50) {
-    for (let y = 0; y <= height; y += 50) {
+  stroke(220);
+  strokeWeight(10);
+  for ( let i = 0; i < bgPoints.trail.length; i++ ) {
+    let pastPoints = bgPoints.trail[i];
+    point(pastPoints.x, pastPoints.y);
+  }
+  bgPoints.x = mouseX;
+  bgPoints.y = mouseY;
+  point(bgPoints.x, bgPoints.y);
+  let newTrailPosition = {
+    x: bgPoints.x, 
+    y: bgPoints.y,
+  };
 
-      let xAngle = map(mouseX, 0, width, PI, 4 * PI, true);
-      let yAngle = map(mouseY, 0, height, -PI, PI, true);
-      let angle = xAngle * (x / width) + yAngle * (y / height);
-      let circleX = x + 20 * cos(2 * PI * t + angle);
-      let circleY = y + 20 * sin(2 * PI * t + angle);
-      fill(255,255,255,100);
-      ellipse(circleX, circleY, 100);
-    }
-  }
-  t = t + 0.01; // code from P5 library ends here
+  bgPoints.trail.push(newTrailPosition);
   pop();
-  // Player position controlled by the mouse
-  push();
-  player.x = mouseX;
-  player.y = mouseY;
-  ellipse(player.x, player.y, player.size);
-  pop();
-  // Lover's circle movements
-  push();
-  lover.x = width/2 + 100 * cos(2 * PI * t + 360);
-  lover.y = height/2 + 100 * sin(2 * PI * t + 360);
-  stroke(255);
-  strokeWeight(20);
-  noFill();
-  ellipse(lover.x, lover.y, lover.size);
-  pop();
-  // Ennemy's circle moving on y axis up and down according to Perlin Noise variables
-  push();
-  ennemy.y = map(noise(t), 0, 1, 0, height);
-  ennemy.x = width/2;
-  fill(40,40,114);
-  ellipse(ennemy.x, ennemy.y, ennemy.size);
-  pop();
-  // To see if the player is in the lover's circle and if it touches the ennemy
-  let dPlayerEnnemy = dist(player.x, player.y, ennemy.x, ennemy.y);
-  let dPlayerLover = dist(player.x, player.y, lover.x, lover.y);
-  let dEnnemyLover = dist(ennemy.x, ennemy.y, lover.x, lover.y);
-  if(dPlayerEnnemy > ennemy.size/2 + player.size/2 && dPlayerLover < lover.size/2 ) {
-    state = 'ending';
-  }
-  else if (dPlayerEnnemy < ennemy.size/2 + player.size/2) {
-    state = `ending2`;
-  }
+  
 }
 
 // Good ending
