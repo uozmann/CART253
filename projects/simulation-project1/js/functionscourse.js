9f7d4d6f2fbe5ada5676bd16323d852cf9ba1719
uozmann <1974155655@qq.com>
2021-10-21 16:17:43 -0400

    Added bubbles interractions

--- projects/simulation-project1/js/functionscourse.js
+++ projects/simulation-project1/js/functionscourse.js
@@ -1,37 +1,115 @@
 "use strict";
+// let bgPoints = {
+//   x: 0,
+//   y: 0,
+//   vx: 0,
+//   vy: 0,
+//   speed: 5,
+//   nbr: 2000,
+//   trail: [],
+// }
 
-let school = [];
-let schoolSize = 4;
+let steppingGrounds = [];
+let nbrSteppingGrounds = 5;
+
+function preload() {
+}
 
 function setup() {
-  createCanvas(600, 600);
-
-  for (let i = 0; i < schoolSize; i++) {
-    // Create a fish
-    let fish = createFish(random(0, width), random(0, height));
-    // Add the fish to our array
-    school.push(fish);
+  createCanvas(1000, 700);
+  for ( let i = 0; i < nbrSteppingGrounds; i++) {
+    let steppingGround = {
+      nbr: 10,
+      x: random(0, width),
+      y: random(0, height),
+      size: 100,
+      vx: 2,
+      vy: 2,
+    }
+    steppingGrounds.push(steppingGround);
   }
 }
-// mousePressed() checks whether a fish in the school was clicked
-// and removes it if so
-function mousePressed() {
-  // Use a for loop to examine every fish in the school one by one
-  for (let i = 0; i < school.length; i++) {
-    // Store the current fish in the fish variable
-    let fish = school[i];
-    // Calculate the distance between the mouse position and the fish
-    let d = dist(mouseX, mouseY, fish.x, fish.y);
-    // If the distance means the mouse was clicked inside the fish
-    if (d < fish.size / 2) {
-      // Remove the fish using the splice() function which takes two arguments
-      // - The index to start remove elements from (i in our case)
-      // - The number of elements to remove from that position (just one for us)
-      school.splice(i, 1);
-      // Now that we've found our fish to remove, we don't want to continue
-      // going through the loop, so we end it prematurely with break
-      // This forces the for-loop to stop immediately
-      break;
+
+function draw() {
+  game();
+}
+
+function game() {
+  background(240, 220, 220);
+  fill(255);
+  
+  for ( let i = 0; i < steppingGrounds.length; i++) {
+    let steppingGround = steppingGrounds[i];
+    steppingGround.x += steppingGround.vx;
+    steppingGround.y += steppingGround.vy;
+
+    steppingGround.x = constrain(steppingGround.x, 0, width);
+    steppingGround.y = constrain(steppingGround.y, 0, height);
+    ellipse(steppingGround.x, steppingGround.y, steppingGround.size);
+
+    if(steppingGround.x > 950 || steppingGround.x < 50) {
+    steppingGround.vx = -steppingGround.vx;
+    }
+    if(steppingGround.y > 650 || steppingGround.y < 50) {
+    steppingGround.vy = -steppingGround.vy;
+    }
+
+  }
+
+  // Balls
+  // push();
+  // fill (255);
+  // steppingGround.x += steppingGround.vx;
+  // steppingGround.y += steppingGround.vy;
+
+  // steppingGround.x = constrain(steppingGround.x, 0, width);
+  // steppingGround.y = constrain(steppingGround.y, 0, height);
+  // ellipse(steppingGround.x, steppingGround.y, steppingGround.size);
+
+  // if(steppingGround.x === 950 || steppingGround.x === 50) {
+  //   steppingGround.vx = -steppingGround.vx;
+  // }
+  // if(steppingGround.y === 650 || steppingGround.y === 50) {
+  //   steppingGround.vy = -steppingGround.vy;
+  }
+  // for ( let i = 0; i < bubbles.length; i++) {
+  //   displayBubbles(bubbles[i]);
+  //   moveBubbles(bubbles[i]);
+    
+  // }
+  
+ 
+  // steppingGround.bubbles.push(newBubblePosition);
+//   pop();
+// }
+
+function title() {
+  background(50);
+  fill(255);
+  textAlign(CENTER, CENTER);
+
+  // Background animation
+  push();
+  stroke(240, 220, 160);
+  strokeWeight(3);
+
+  bgPoints.x = mouseX;
+  bgPoints.y = mouseY;
+  point(bgPoints.x, bgPoints.y);
+  let newTrailPosition = {
+    x: bgPoints.x, 
+    y: bgPoints.y,
+  };
+  bgPoints.trail.push(newTrailPosition);
+
+  if (bgPoints.trail.length>=2) {
+    // point(bgPoints.trail[0].x, bgPoints.trail[0].y);
+    for ( let i = 0; i < bgPoints.trail.length-1; i++ ) {
+      let pastPoints = bgPoints.trail[i];
+      let nextPoints = bgPoints.trail[i+1];
+      // point(nextPoints.x, nextPoints.y);
+      line(pastPoints.x, pastPoints.y, nextPoints.x, nextPoints.y);
     }
   }
+  pop();
 }
--- projects/simulation-project1/js/script.js
+++ projects/simulation-project1/js/script.js
@@ -5,7 +5,7 @@
 // Fonts
 let cairoBlack;
 let cairoRegular;
-let state = 'title'
+let state = 'title';
 let bgPoints = {
   x: 0,
   y: 0,
@@ -14,14 +14,11 @@ let bgPoints = {
   speed: 5,
   nbr: 2000,
   trail: [],
+  player: [],
 }
-let bgAutoPoints = {
+let bgPlayerPoints = {
   x: 0,
   y: 0,
-  vx: 0,
-  vy: 0,
-  speed: 5,
-  nbr: 2000,
   trail: [],
 }
 
@@ -41,15 +38,32 @@ let player = {
   speed: 0.01,
 }
 
+let steppingGrounds = [];
+let nbrSteppingGrounds = 5;
+
+let bubbles = [];
+let bubblesSize = 10;
+
 // Loading images and text font
 function preload() {
   cairoBlack = loadFont(`assets/fonts/Cairo/Cairo-Black.ttf`);
   cairoRegular = loadFont(`assets/fonts/Cairo/Cairo-Regular.ttf`);
-  // bgGame = loadImage(`assets/images/cloudgirl-bg.jpg`)
 }
 
 function setup() {
   createCanvas(1000, 700);
+  for ( let i = 0; i < nbrSteppingGrounds; i++) {
+    
+    let steppingGround = {
+      nbr: 10,
+      x: random(0, width-100),
+      y: random(0, height-100),
+      size: 100,
+      vx: 2,
+      vy: 2,
+    }
+    steppingGrounds.push(steppingGround);
+  }
 }
 
 function draw() {
@@ -75,8 +89,9 @@ function title() {
 
   // Background animation
   push();
-  stroke(240, 220, 160);
+  stroke(220, 200, 200);
   strokeWeight(3);
+
   for ( let i = 0; i < bgPoints.trail.length; i++ ) {
     let pastPoints = bgPoints.trail[i];
     point(pastPoints.x, pastPoints.y);
@@ -90,17 +105,13 @@ function title() {
     y: bgPoints.y,
   };
   bgPoints.trail.push(newTrailPosition);
-
-  // for ( let j = 0; j < bgPoints.trail.length; j++ ) {
-  //   line(pastPoints.x, pastPoints.y, bgPoints.x, bgPoints.y);
-  // }
   pop();
 
   // Title text
   push();
   textFont(cairoBlack);
   textSize(100);
-  text(`A simple life story`, width / 2, height / 3);
+  text(`Bubble Canvas`, width / 2, height / 3);
   pop();
 
   // Start command text
@@ -130,19 +141,20 @@ function game() {
   // Background animation 
   push();
   stroke(220, 200, 200);
-  strokeWeight(10);
+  strokeWeight(1);
+
   for ( let i = 0; i < bgPoints.trail.length; i++ ) {
     let pastPoints = bgPoints.trail[i];
     point(pastPoints.x, pastPoints.y);
+    line(pastPoints.x, pastPoints.y, bgPoints.x, bgPoints.y);
   }
-  bgPoints.x = mouseX;
-  bgPoints.y = mouseY;
+  bgPoints.x = player.x;
+  bgPoints.y = player.y;
   point(bgPoints.x, bgPoints.y);
   let newTrailPosition = {
     x: bgPoints.x, 
     y: bgPoints.y,
   };
-
   bgPoints.trail.push(newTrailPosition);
   pop();
   
@@ -151,6 +163,9 @@ function game() {
   fill(0);
   player.vy += player.speed;
   player.y = player.y + player.vy;
+  player.y = constrain(player.y, 0, height-50);
+  player.x = constrain(player.x, 0, width-50);
+
   ellipse(player.x, player.y, player.size);
   if (keyIsDown(65)) {
     player.x += -5;
@@ -158,63 +173,89 @@ function game() {
   if (keyIsDown(68)) {
     player.x += 5;
   }
+  
+  // Player going up a little bit when touching the bubbles
+  for ( let i = 0; i < steppingGrounds.length; i++) {
+    let steppingGround = steppingGrounds[i];
+    let dPlayerBubbles = dist(steppingGround.x, steppingGround.y, player.x, player.y);
+    if(dPlayerBubbles < player.size/2 + steppingGround.size/2) {
+      player.vy = -2;
+    }
+    if (player.y < 25) {
+      player.vy += player.speed;
+    }
+  }
 
-  if(player.y > gameDangerZone.y) {
+  // Conditions for end of the game
+  if(player.y >= gameDangerZone.y) {
     state = 'ending';
   }
   pop();
-}
 
-// Good ending
+  // Balls bounding on the screen
+  push();
+  for ( let i = 0; i < steppingGrounds.length; i++) {
+    let steppingGround = steppingGrounds[i];
+    steppingGround.x += steppingGround.vx;
+    steppingGround.y += steppingGround.vy;
+
+    steppingGround.x = constrain(steppingGround.x, 0, width);
+    steppingGround.y = constrain(steppingGround.y, 0, height);
+    ellipse(steppingGround.x, steppingGround.y, steppingGround.size);
+
+    if(steppingGround.x > 950 || steppingGround.x < 50) {
+      steppingGround.vx = -steppingGround.vx;
+    }
+    if (steppingGround.y > 650 || steppingGround.y < 50) {
+      steppingGround.vy = -steppingGround.vy;
+    }
+
+    let dBubbles = dist(steppingGround.x, steppingGround.y, steppingGround.x, steppingGround.y);
+    if(dBubbles <= steppingGround.size) {
+      steppingGround.vy += 0.2;
+    }
+  }
+
+  pop();
+  }
+  
+
+
+
+// Ending
 function ending(){
-  background(255,255,255);
-  fill(40,40,114);
+  background(50);
+  fill(240, 220, 220);
+
+  push();
+  stroke(120, 100, 100);
+  strokeWeight(3);
+
+  for ( let i = 0; i < bgPoints.trail.length; i++ ) {
+    let pastPoints = bgPoints.trail[i];
+    point(pastPoints.x, pastPoints.y);
+    line(pastPoints.x, pastPoints.y, bgPoints.x, bgPoints.y);
+  }
+  bgPoints.x = player.x;
+  bgPoints.y = player.y;
+  point(bgPoints.x, bgPoints.y);
+  let newTrailPosition = {
+    x: bgPoints.x, 
+    y: bgPoints.y,
+  };
+  bgPoints.trail.push(newTrailPosition);
+  pop();
 
   push();
   textFont(cairoBlack);
-  textSize(200);
-  text(`Congradulations!`, width / 2, height / 3);
+  textSize(100);
+  text(`End`, width / 2, height / 3);
   pop();
 
   push();
   textFont(cairoRegular);
   textSize(24);
-  text(`You found your lover!!!`, width/2, height*2 / 3);
-  text(`Press "R" to restart`, width/2, height*3/4);
+  text(`You touched the ground`, width/2, height*2 / 3);
+  text(`Press "CTRL + R" to restart`, width/2, height*3/4);
   pop();
-
-  noLoop();
-}
-
-// Bad ending
-function ending2(){
-  background(0,0,0);
-  fill(40,40,114);
-
-  push();
-  textFont(cairoBlack);
-  textSize(200);
-  text(`You Lost!`, width / 2, height / 3);
-  pop();
-
-  push();
-  textFont(cairoRegular);
-  textSize(24);
-  text(`You have been interupted by your ennemy!!!`, width/2, height*2 / 3);
-  text(`Press "R" to restart`, width/2, height*3/4);
-  pop();
-
-  noLoop();
-}
-
-// Return to the title page
-// function keyIsDown() {
-//   if (key === 'r') {
-//     state = 'title';
-//     loop();
-//   }
-//   if (key ==='a') {
-//     player.x += -1
-//   }
-//   if 
-// }
+} 
